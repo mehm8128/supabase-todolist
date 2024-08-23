cd4c6aa31860024fedd222c75a3e71e54fa3f1df
mehm8128 <mehm8128@gmail.com>
2024-08-23 22:55:38 +0900

    remove neverthrow

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,11 +1,7 @@
-import { ResultAsync } from 'neverthrow'
-
-const _fetcher = <T>(url: string, options?: RequestInit): Promise<T> =>
+export const fetcher = <T>(url: string, options?: RequestInit): Promise<T> =>
 	fetch(url, options).then<T>(res => {
 		if (!res.ok) {
 			throw new Error(res.statusText)
 		}
 		return res.json()
 	})
-
-export const fetcher = ResultAsync.fromThrowable(_fetcher)
